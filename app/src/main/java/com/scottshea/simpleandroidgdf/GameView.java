36b1722843b288927e9d3d0031a12572e18f0cec
TreePelvis <scott-shea@hotmail.com>
2016-02-13 10:10:29 -0500

    Initial commit - Game View and GameMainActivity started

---
+++ app/src/main/java/com/scottshea/simpleandroidgdf/GameView.java
@@ -0,0 +1,17 @@
+package com.scottshea.simpleandroidgdf;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+/**
+ * Created by Scott on 2/12/2016.
+ */
+public class GameView extends SurfaceView {
+    public GameView(Context context, int gameWidth, int gameHeight) {
+        super(context);
+    }
+
+    public GameView(Context context) {
+        super(context);
+    }
+}
